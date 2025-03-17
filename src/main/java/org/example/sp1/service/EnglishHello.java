51af944778bfd54b223499c6adf4149c2f94090e
zerock <kangckck24@gmail.com>
2025-03-17 14:29:19 +0900

    0314

--- src/main/java/org/example/sp1/service/EnglishHello.java
+++
@@ -1,8 +0,0 @@
-package org.example.sp1.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class EnglishHello implements HelloService {
-
-}
--- src/test/java/org/example/db/DBTests.java
+++ src/test/java/org/example/db/DBTests.java
@@ -1,19 +1,51 @@
 package org.example.db;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.log4j.Log4j2;
+import org.example.sp1.service.HelloService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.Connection;
 
 @ExtendWith(SpringExtension.class)
 @Log4j2
 @ContextConfiguration("file:src/main/webapp/WEB-INF/root-context.xml")
 public class DBTests {
 
+    @Autowired
+    HikariDataSource ds;
+
     @Test
     public void testDB() {
         log.info("testDB");
+        log.info(ds);
+    }
+
+    @Test
+    public void testDB2() throws Exception{
+
+        Connection conn = ds.getConnection();
+
+        log.info(conn);
+
+        conn.close();
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+

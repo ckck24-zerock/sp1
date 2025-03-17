5288c48769de3efc9dbf7fec6f8d91226aa2401d
zerock <kangckck24@gmail.com>
2025-03-17 15:43:02 +0900

    0314

--- src/test/java/org/example/db/DBTests.java
+++ src/test/java/org/example/db/DBTests.java
@@ -2,6 +2,7 @@ package org.example.db;
 
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.log4j.Log4j2;
+import org.example.sp1.mapper.TimeMapper;
 import org.example.sp1.service.HelloService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
 @Log4j2
@@ -18,6 +20,15 @@ public class DBTests {
 
     @Autowired
     HikariDataSource ds;
+
+    @Autowired(required = false)
+    TimeMapper timeMapper;
+
+    @Test
+    public void testTime() throws SQLException {
+        log.info(timeMapper.getTime());
+    }
+
 
     @Test
     public void testDB() {

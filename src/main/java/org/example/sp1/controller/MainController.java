99192e8d23446681c5d17e02307b944160d6b30e
zerock <kangckck24@gmail.com>
2025-03-18 15:49:25 +0900

    0318

---
+++ src/main/java/org/example/sp1/controller/MainController.java
@@ -0,0 +1,18 @@
+package org.example.sp1.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class MainController {
+
+    @GetMapping("/")
+    public String main(){
+
+        log.info("MainController main");
+
+        return "main";
+    }
+}

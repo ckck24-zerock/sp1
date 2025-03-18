f35c217284c7080b344f82604beb99758fb170f4
zerock <kangckck24@gmail.com>
2025-03-18 10:10:50 +0900

    0318

---
+++ src/main/java/org/example/sp1/dto/BoardDTO.java
@@ -0,0 +1,13 @@
+package org.example.sp1.dto;
+
+import java.time.LocalDateTime;
+
+public class BoardDTO {
+
+    private Integer bno;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+}
